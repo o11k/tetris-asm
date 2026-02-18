4cd20c5e42b6547272bbdd9389a4f6616c7ea5ac
Ori Levenglick <61731312+o11k@users.noreply.github.com>
2026-02-18 17:06:57 -0600

    Controls from raw keyboard input

--- src/tetris.asm
+++ src/tetris.asm
@@ -1,14 +1,7 @@
     org 100h
     use16
 
-MINO_NONE EQU 00h
-MINO_L    EQU 01h
-MINO_J    EQU 02h
-MINO_S    EQU 03h
-MINO_Z    EQU 04h
-MINO_T    EQU 05h
-MINO_O    EQU 06h
-MINO_I    EQU 07h
+include 'constants.inc'
 
 start:
     push bp
@@ -23,39 +16,94 @@ start:
     call draw_matrix
 
 .loop_main:
-    call get_accurate_time
-    push cx
-    push dx
-    push ax
+    call get_controls_state
+    mov  si, ax
 
-    push 20
-    push 10
+    ; Esc = exit
+    mov  al, byte [si+CONTROLS_STATE_OFF_PAUSE]
+    test al, KEYBOARD_STATE_MASK_WAS_PRESSED
+    jnz  .loop_main_end
+
+    ; Rotate right
+    mov  al, byte [si+CONTROLS_STATE_OFF_ROT_RIGHT]
+    test al, KEYBOARD_STATE_MASK_WAS_PRESSED
+    jz   .rotate_right_false
+.rotate_right:
+    inc word [pointer_value]
+    and word [pointer_value], 3
+.rotate_right_false:
+
+    ; Rotate left
+    mov  al, byte [si+CONTROLS_STATE_OFF_ROT_LEFT]
+    test al, KEYBOARD_STATE_MASK_WAS_PRESSED
+    jz   .rotate_left_false
+.rotate_left:
+    dec word [pointer_value]
+    and word [pointer_value], 3
+.rotate_left_false:
+
+    ; Rotate 180 degrees
+    mov  al, byte [si+CONTROLS_STATE_OFF_ROT_180]
+    test al, KEYBOARD_STATE_MASK_WAS_PRESSED
+    jz   .rotate_180_false
+.rotate_180:
+    add word [pointer_value], 2
+    and word [pointer_value], 3
+.rotate_180_false:
+
+    ; Move right
+    mov  al, byte [si+CONTROLS_STATE_OFF_RIGHT]
+    test al, KEYBOARD_STATE_MASK_WAS_PRESSED
+    jz   .right_false
+.right:
+    add word [pointer_pos_x], 8
+.right_false:
+
+    ; Move left
+    mov  al, byte [si+CONTROLS_STATE_OFF_LEFT]
+    test al, KEYBOARD_STATE_MASK_WAS_PRESSED
+    jz   .left_false
+.left:
+    sub word [pointer_pos_x], 8
+.left_false:
+
+    ; skip if no inputs
+    mov ax, word [pointer_value]
+    cmp ax, word [old_pointer_value]
+    jne .changed
+    mov ax, word [pointer_pos_x]
+    cmp ax, word [old_pointer_pos_x]
+    jne .changed
+    jmp .no_change
+
+.changed:
+    ; delete old
+    push 20  ;y
+    push word [old_pointer_pos_x]
     push 00h
-    push word_hex_buffer
+    push pointer_str
     call render_text
     add sp, 8
 
-    push word_hex_buffer + 0
-    push word [bp-2]
-    call word_to_hex
-    add  sp, 4
-    push word_hex_buffer + 4
-    push word [bp-4]
-    call word_to_hex
-    add  sp, 4
-    push word_hex_buffer + 8
-    push word [bp-6]
-    call word_to_hex
-    add  sp, 4
+    ; update values
+    mov ax, word [pointer_value]
+    mov word [old_pointer_value], ax
+    mov ax, word [pointer_pos_x]
+    mov word [old_pointer_pos_x], ax
 
-    push 20
-    push 10
+    mov si, [pointer_value]
+    mov al, byte [pointer_chars+si]
+    mov byte [pointer_str], al
+
+    ; render new
+    push 20  ;y
+    push word [pointer_pos_x]
     push 0Fh
-    push word_hex_buffer
+    push pointer_str
     call render_text
     add sp, 8
 
-    add sp, 6
+.no_change:
     jmp .loop_main
 .loop_main_end:
 
@@ -64,15 +112,19 @@ start:
     mov ax, 4C00h
     int 21h
 
-
-word_hex_buffer: db 0,0,0,0, 0,0,0,0, 0,0,0,0, 0
-
+old_pointer_value: dw 1
+pointer_value: dw 0
+pointer_str: db "^", 0
+pointer_chars: db "^>v<", 0
+old_pointer_pos_x: dw 21
+pointer_pos_x: dw 20
 
 include 'render.inc'
 include 'render_text.inc'
 include 'keyboard_input.inc'
 include 'misc.inc'
 include 'time.inc'
+include 'controls.inc'
 
 ; appears upside-down
 matrix db \
