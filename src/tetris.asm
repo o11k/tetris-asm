c0ea66dd1275c71aeeb46c706e48d81c2cd996ae
Ori Levenglick <61731312+o11k@users.noreply.github.com>
2026-02-19 15:24:55 -0600

    Tetrimino generation

--- src/tetris.asm
+++ src/tetris.asm
@@ -7,117 +7,75 @@ start:
     push bp
     mov bp, sp
 
+.setup:
     call register_int_09h
 
     ; video mode 13h
     mov ax, 0013h
     int 10h
 
+    ; seed prng
+    call get_accurate_time
+    xor ax, cx
+    xor ax, dx
+    push ax
+    call prng_seed
+    add sp, 2
+
+.main:
     call draw_matrix
 
-.loop_main:
+.main_loop:
     call get_controls_state
     mov  si, ax
 
     ; Esc = exit
     mov  al, byte [si+CONTROLS_STATE_OFF_PAUSE]
     test al, KEYBOARD_STATE_MASK_WAS_PRESSED
-    jnz  .loop_main_end
+    jnz  .main_loop_end
 
-    ; Rotate right
-    mov  al, byte [si+CONTROLS_STATE_OFF_ROT_RIGHT]
+    ; Space = generate
+    mov  al, byte [si+CONTROLS_STATE_OFF_HARD_DROP]
     test al, KEYBOARD_STATE_MASK_WAS_PRESSED
-    jz   .rotate_right_false
-.rotate_right:
-    inc word [pointer_value]
-    and word [pointer_value], 3
-.rotate_right_false:
+    jnz  .generate
 
-    ; Rotate left
-    mov  al, byte [si+CONTROLS_STATE_OFF_ROT_LEFT]
-    test al, KEYBOARD_STATE_MASK_WAS_PRESSED
-    jz   .rotate_left_false
-.rotate_left:
-    dec word [pointer_value]
-    and word [pointer_value], 3
-.rotate_left_false:
-
-    ; Rotate 180 degrees
-    mov  al, byte [si+CONTROLS_STATE_OFF_ROT_180]
-    test al, KEYBOARD_STATE_MASK_WAS_PRESSED
-    jz   .rotate_180_false
-.rotate_180:
-    add word [pointer_value], 2
-    and word [pointer_value], 3
-.rotate_180_false:
-
-    ; Move right
-    mov  al, byte [si+CONTROLS_STATE_OFF_RIGHT]
-    test al, KEYBOARD_STATE_MASK_WAS_PRESSED
-    jz   .right_false
-.right:
-    add word [pointer_pos_x], 8
-.right_false:
-
-    ; Move left
-    mov  al, byte [si+CONTROLS_STATE_OFF_LEFT]
-    test al, KEYBOARD_STATE_MASK_WAS_PRESSED
-    jz   .left_false
-.left:
-    sub word [pointer_pos_x], 8
-.left_false:
-
-    ; skip if no inputs
-    mov ax, word [pointer_value]
-    cmp ax, word [old_pointer_value]
-    jne .changed
-    mov ax, word [pointer_pos_x]
-    cmp ax, word [old_pointer_pos_x]
-    jne .changed
-    jmp .no_change
-
-.changed:
+    jmp .skip_generate
+.generate:
     ; delete old
-    push 20  ;y
-    push word [old_pointer_pos_x]
+    push 20
+    push 20
     push 00h
-    push pointer_str
+    push mino_str_buffer
     call render_text
-    add sp, 8
+    add  sp, 8
 
-    ; update values
-    mov ax, word [pointer_value]
-    mov word [old_pointer_value], ax
-    mov ax, word [pointer_pos_x]
-    mov word [old_pointer_pos_x], ax
+    ; generate
+    call next_tetrimino
+    push mino_str_buffer
+    push ax
+    call word_to_hex
+    add  sp, 4
 
-    mov si, [pointer_value]
-    mov al, byte [pointer_chars+si]
-    mov byte [pointer_str], al
-
-    ; render new
-    push 20  ;y
-    push word [pointer_pos_x]
+    ; write new
+    push 20
+    push 20
     push 0Fh
-    push pointer_str
+    push mino_str_buffer
     call render_text
-    add sp, 8
+    add  sp, 8
 
-.no_change:
-    jmp .loop_main
-.loop_main_end:
+.skip_generate:
+    jmp .main_loop
+.main_loop_end:
 
+.teardown:
     call restore_int_09h
     ; exit 0
     mov ax, 4C00h
     int 21h
 
-old_pointer_value: dw 1
-pointer_value: dw 0
-pointer_str: db "^", 0
-pointer_chars: db "^>v<", 0
-old_pointer_pos_x: dw 21
-pointer_pos_x: dw 20
+
+mino_str_buffer: db 0,0,0,0,0
 
 include 'input/time.inc'
 include 'input/keyboard_input.inc'
@@ -125,6 +83,8 @@ include 'input/controls.inc'
 include 'output/render.inc'
 include 'output/render_text.inc'
 include 'misc.inc'
+include 'logic/prng.inc'
+include 'logic/tetrimino_generation.inc'
 
 ; appears upside-down
 matrix db \
