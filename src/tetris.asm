67a9676019f8767dcd5927dd5714193529137195
Ori Levenglick <61731312+o11k@users.noreply.github.com>
2026-02-19 19:13:33 -0600

    Collision detection and movement

--- src/tetris.asm
+++ src/tetris.asm
@@ -25,22 +25,6 @@ start:
 .main:
     call draw_matrix
 
-.main_loop:
-    call get_controls_state
-    mov  si, ax
-
-    ; Esc = exit
-    mov  al, byte [si+CONTROLS_STATE_OFF_PAUSE]
-    test al, KEYBOARD_STATE_MASK_WAS_PRESSED
-    jnz  .main_loop_end
-
-    ; Space = generate
-    mov  al, byte [si+CONTROLS_STATE_OFF_HARD_DROP]
-    test al, KEYBOARD_STATE_MASK_WAS_PRESSED
-    jnz  .generate
-
-    jmp .skip_generate
-.generate:
     call next_mino
     push tetrimino_buffer
     push ax
@@ -50,7 +34,69 @@ start:
     call draw_matrix_tetrimino
     add sp, 2
 
+.main_loop:
+    call get_controls_state
+    mov  si, ax
+
+    ; Esc = exit
+    mov  al, byte [si+CONTROLS_STATE_OFF_PAUSE]
+    test al, KEYBOARD_STATE_MASK_WAS_PRESSED
+    jnz  .main_loop_end
+
+    ; No space/arrows = next frame
+    xor  al, al
+    or   al, byte [si+CONTROLS_STATE_OFF_LEFT]
+    or   al, byte [si+CONTROLS_STATE_OFF_RIGHT]
+    or   al, byte [si+CONTROLS_STATE_OFF_ROT_RIGHT]
+    or   al, byte [si+CONTROLS_STATE_OFF_SOFT_DROP]
+    or   al, byte [si+CONTROLS_STATE_OFF_HARD_DROP]
+    test al, KEYBOARD_STATE_MASK_WAS_PRESSED
+    jz   .skip_everything
+
+    ; Delete old tetrimino
+    push tetrimino_buffer
+    call delete_matrix_tetrimino
+    add sp, 2
+
+    ; Space = generate
+    mov  al, byte [si+CONTROLS_STATE_OFF_HARD_DROP]
+    test al, KEYBOARD_STATE_MASK_WAS_PRESSED
+    jz   .skip_generate
+
+.generate:
+    call next_mino
+    push tetrimino_buffer
+    push ax
+    call initialize_tetrimino
+    add sp, 4
 .skip_generate:
+
+
+macro do_move name, key, row, col {
+    mov  al, byte [si+CONTROLS_STATE_OFF_#key]
+    test al, KEYBOARD_STATE_MASK_WAS_PRESSED
+    jz   .skip_#name
+
+    push col
+    push row
+    push tetrimino_buffer
+    push matrix
+    call tetrimino_try_move
+    add sp, 8
+.skip_#name:
+}
+
+    do_move left  ,LEFT      , 0 ,-1
+    do_move right ,RIGHT     , 0 , 1
+    do_move up    ,ROT_RIGHT , 1 , 0
+    do_move down  ,SOFT_DROP ,-1 , 0
+
+    ; draw new tetrimino
+    push tetrimino_buffer
+    call draw_matrix_tetrimino
+    add sp, 2
+
+.skip_everything:
     jmp .main_loop
 .main_loop_end:
 
@@ -62,6 +108,7 @@ start:
 
 
 tetrimino_buffer: times TETRIMINO_SIZE db 0
+word_str_buffer: db 0,0,0,0,0
 
 include 'input/time.inc'
 include 'input/keyboard_input.inc'
@@ -92,7 +139,7 @@ matrix db \
     2,0,0,0,0,0,0,0,0,2, \
     2,0,0,0,0,0,0,0,0,2, \
     2,0,0,0,0,0,0,0,0,2, \
+    2,4,4,4,0,0,0,0,0,2, \
     2,0,0,0,0,0,0,0,0,2, \
-    2,0,0,0,0,0,0,0,0,2, \
-    3,3,3,3,3,3,3,3,3,3, \
+    3,0,0,0,0,0,0,0,0,3, \
     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
